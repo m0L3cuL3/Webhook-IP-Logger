8c8359fc6f63aa557ab7fc755fcbccd33b39d7ae
m0L3cuL3 <skidrowloler@gmail.com>
2021-09-10 23:39:44 +0800

    removed webhook link

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  Last update : 29/03/2020
 */
 ini_set('display_errors','off');
-/*<------------- Placer le lien webhook ici ------------------>*/ $webhook = "INSERT WEBHOOK HERE";
+$webhook = "webhook here";
 $user_agent     =   $_SERVER['HTTP_USER_AGENT'];
 function getOS() { 
     global $user_agent;
